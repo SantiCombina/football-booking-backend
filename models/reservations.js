56ca0c10341110ae5161ee52c11f1c6533eb44a1
Mateo Koffler <mkoffler8@gmail.com>
2024-11-05 19:37:46 -0300

    Validacion de reservas por horarios

--- models/reservations.js
+++ models/reservations.js
@@ -26,6 +26,13 @@ module.exports = (sequelize, DataTypes) => {
         type: DataTypes.DATE,
         allowNull: false,
       },
+      startTime: {
+        type: DataTypes.TIME,
+        allowNull: false,
+        validate: {
+          isIn: [["16:00", "17:00", "18:00", "19:00", "20:00", "21:00", "22:00", "23:00"]],
+        },
+      },
       id_user: {
         type: DataTypes.INTEGER,
         allowNull: false,
