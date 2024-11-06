11c3fb42b69c811060c456dcd145797b392a54a1
SantiCombina <santicombina@gmail.com>
2024-11-06 18:16:18 -0300

    Add migrations for Users, Fields, and Reservations tables

---
+++ migrations/20241030230449-create-fields.js
@@ -0,0 +1,37 @@
+"use strict";
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("Fields", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      name: {
+        type: Sequelize.STRING,
+      },
+      description: {
+        type: Sequelize.STRING,
+      },
+      direction: {
+        type: Sequelize.STRING,
+      },
+      photo: {
+        type: Sequelize.STRING,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("Fields");
+  },
+};
---
+++ migrations/20241030230847-create-reservations.js
@@ -0,0 +1,54 @@
+"use strict";
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("Reservations", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      date: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      startTime: {
+        type: Sequelize.TIME,
+        allowNull: false,
+        validate: {
+          isIn: [["16:00", "17:00", "18:00", "19:00", "20:00", "21:00", "22:00"]],
+        },
+      },
+      id_user: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: "Users",
+          key: "id",
+        },
+      },
+      id_field: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: "Fields",
+          key: "id",
+        },
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.NOW,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.NOW,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("Reservations");
+  },
+};
